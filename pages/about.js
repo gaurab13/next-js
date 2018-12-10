b2ceeefcade9cb52cbfd31779630108f511a98aa
Gaurab KC <13gaurab@gmail.com>
2018-12-10 15:33:01 +0545

    Add express server and components

---
+++ pages/about.js
@@ -0,0 +1,6 @@
+import Layout from '../components/Layout.js'
+export default () => (
+  <Layout>
+    <p>This is the about page</p>
+  </Layout>
+)
---
+++ pages/index.js
@@ -0,0 +1,59 @@
+import Layout from '../components/Layout.js'
+import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
+
+const PostLink = (props) => (
+  <li>
+    <Link as={`/p/${props.id}`} href={`/post?id=${props.id}`}>
+      <a>{props.title}</a>
+    </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </li>
+)
+
+const Index = (props) => (
+  <Layout>
+    <h1>My Blog</h1>
+    <ul>
+      {props.shows.map(({show}) => (
+        <PostLink key={show.id} id={show.id} title={show.name} />
+      ))}
+    </ul>
+    <style jsx>{`
+      h1, a {
+        font-family: "Arial";
+      }
+
+      ul {
+        padding: 0;
+      }
+    `}</style>
+  </Layout>
+)
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+  const data = await res.json()
+
+  console.log(`Show data fetched. Count: ${data.length}`)
+
+  return {
+    shows: data
+  }
+}
+
+export default Index
